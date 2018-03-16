d89e202a6496ecefc8dc46fc5ad99f7febd731d5
ulnagar <ulnagar@users.noreply.github.com>
2018-03-16 23:27:25 +1100

    Test Single array

--- MMM-FortnightSchedule.js
+++ MMM-FortnightSchedule.js
@@ -10,7 +10,14 @@
 Module.register("MMM-FortnightSchedule", {
 	defaults: {
 		updateInterval: 60000,
-		retryDelay: 5000
+		retryDelay: 5000,
+		config: {
+			timeslots: [ "Period 1", "Period 2"],
+			schedule: {
+				a: [ "Week A Activity 1", "Week A Activity 2"],
+				b: [ "Week B Activity 1", "Week B Activity 2"]
+			}
+		}
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
@@ -30,6 +37,7 @@ Module.register("MMM-FortnightSchedule", {
 		// create element wrapper for show into the module
 		var wrapper = document.createElement("div");
 		wrapper.innerHTML = "This is the content!";
+		wrapper.innerHTML = this.defaults.config.schedule.a[0];
 	
 		return wrapper;
 	},
