27190d510144afbf4f44e08169f75d2955de768b
ulnagar <ulnagar@users.noreply.github.com>
2018-03-17 12:22:19 +1100

    Implement moment.js to calculate day and week.

--- MMM-FortnightSchedule.js
+++ MMM-FortnightSchedule.js
@@ -11,16 +11,27 @@ Module.register("MMM-FortnightSchedule", {
 	defaults: {
 		updateInterval: 60000,
 		retryDelay: 5000,
+		yearStartWeek: "b",
 		config: {
 			timeslots: [ "Period 1", "Period 2"],
 			schedule: {
 				a: { 
 					mon: [ "Week A Monday Activity 1", "Week A Monday Activity 2" ],
-					tue: [ "Week A Tuesday Activity 1", "Week A Tuesday Activity 2" ]
+					tue: [ "Week A Tuesday Activity 1", "Week A Tuesday Activity 2" ],
+					wed: [ "Week A Wednesday Activity 1", "Week A Wednesday Activity 2" ],
+					thu: [ "Week A Thursday Activity 1", "Week A Thursday Activity 2" ],
+					fri: [ "Week A Friday Activity 1", "Week A Friday Activity 2" ],
+					sat: [ "Week A Saturday Activity 1", "Week A Saturday Activity 2" ],
+					sun: [ "Week A Sunday Activity 1", "Week A Sunday Activity 2" ]					
 				},
 				b: { 
 					mon: [ "Week B Monday Activity 1", "Week B Monday Activity 2" ],
-					tue: [ "Week B Tuesday Activity 1", "Week B Tuesday Activity 2" ]
+					tue: [ "Week B Tuesday Activity 1", "Week B Tuesday Activity 2" ],
+					wed: [ "Week B Wednesday Activity 1", "Week B Wednesday Activity 2" ],
+					thu: [ "Week B Thursday Activity 1", "Week B Thursday Activity 2" ],
+					fri: [ "Week B Friday Activity 1", "Week B Friday Activity 2" ],
+					sat: [ "Week B Saturday Activity 1", "Week B Saturday Activity 2" ],
+					sun: [ "Week B Sunday Activity 1", "Week B Sunday Activity 2" ]	
 				}
 			}
 		}
@@ -40,22 +51,25 @@ Module.register("MMM-FortnightSchedule", {
 	getDom: function() {
 		var self = this;
 
-		var week = "a";
-		var day = "mon";
+		var week = this.getDisplayWeek().toLowerCase();
+		var day = this.getDisplayDate().locale("en").format("ddd").toLowerCase();
 		var schedule;
-		if(week === "a") {
+
+		if (week === "a") {
 			console.log("WEEK A DETECTED");
 			schedule = this.defaults.config.schedule.a[day];
 			//schedule = this.config.schedule.a[day]; // For final version!
 			console.log("Schedule: ", schedule);
-		} else {
+		}
+		
+		if (week === "b") {
 			console.log("WEEK B DETECTED");
 			schedule = this.defaults.config.schedule.b[day];
 			//schedule = this.config.schedule.b[day]; // For final version!
 			console.log("Schedule: ", schedule);
 		}
 
-		if(schedule === undefined) {
+		if (schedule === undefined) {
 			return "NO DATA FOUND";
 		}
 
@@ -72,6 +86,35 @@ Module.register("MMM-FortnightSchedule", {
 		}
 
 		return wrapper;
+	},
+
+	getDisplayDate: function() {
+		var threshold = moment().startOf("day")
+			.add(moment.duration("16:00"));
+
+		var now = moment();
+		if (now.isAfter(threshold)) {
+			now = now.add(1, "day");
+		}
+
+		return now;
+	},
+
+	getDisplayWeek: function() {
+		var now = this.getDisplayDate();
+		var week = moment(now).isoWeek();
+
+		var yearStartWeek = (this.config.yearStartWeek) ? this.config.yearStartWeek : this.defaults.yearStartWeek;
+
+		if ((week % 2 === 0 && yearStartWeek === "a") || (week % 2 === 1 && yearStartWeek === "b")) {
+			return "b";
+		}
+
+		if ((week % 2 === 0 && yearStartWeek === "b") || (week % 2 === 1 && yearStartWeek === "a")) {
+			return "a";
+		}
+
+		return null;
 	},
 
 	createTimetableRow: function(time, entry) {
@@ -92,7 +135,7 @@ Module.register("MMM-FortnightSchedule", {
 	},
 
 	getScripts: function() {
-		return [];
+		return [ "moment.js" ];
 	},
 
 	getStyles: function () {
